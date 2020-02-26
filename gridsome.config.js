b9ac4447c719c7771acaed67487fbd182c224103
João Vitor Alves <joao.vitor@redealumni.com>
2020-02-26 09:21:28 -0300

    Initial Commit

---
+++ gridsome.config.js
@@ -0,0 +1,45 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'joaovitoras (João Vitor A. S.) blog',
+  siteDescription: 'Blog pessoal para coisas aleatórias que me da vontade de escrever',
+  siteUrl: 'https://joaovitoras.github.io',
+  pathPrefix: '/joaovitoras.github.io',
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
+}
